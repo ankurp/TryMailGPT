6e15b3dcb18b5ac9482fbb93337244be14cf763d
Ankur Patel <ankur@encoredevlabs.com>
2024-10-28 19:00:05 -0700

    Fixing email parsing

--- app/mailboxes/chat_mailbox.rb
+++ app/mailboxes/chat_mailbox.rb
@@ -1,5 +1,10 @@
 class ChatMailbox < ApplicationMailbox
   def process
-    ResponseMailer.send_response(mail.from, mail.subject, mail.decoded).deliver_later
+    body = if mail.multipart?
+      mail.text_part.decoded
+    else
+      mail.decoded
+    end
+    ResponseMailer.send_response(mail.from, mail.subject, body).deliver_later
   end
 end
