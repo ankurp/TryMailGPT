83ce935a464fe8dce0ec30087ad1902e6c3c77d0
Ankur Patel <ankur@encoredevlabs.com>
2024-11-06 15:06:45 -0800

    Updating to reply as email thread

--- app/mailboxes/chat_mailbox.rb
+++ app/mailboxes/chat_mailbox.rb
@@ -1,10 +1,5 @@
 class ChatMailbox < ApplicationMailbox
   def process
-    body = if mail.multipart?
-      mail.text_part.decoded
-    else
-      mail.decoded
-    end
-    ResponseMailer.send_response(mail.from, mail.subject, body).deliver_later
+    ResponseMailer.send_response(mail.from, mail.subject, mail.raw_source).deliver_later
   end
 end
--- app/mailers/response_mailer.rb
+++ app/mailers/response_mailer.rb
@@ -4,14 +4,32 @@ class ResponseMailer < ApplicationMailer
     response = client.chat(
       parameters: {
         model: "gpt-4o",
-        messages: [ { role: "user", content: prompt } ],
-        temperature: 0.7
+        messages: [
+          {
+            role: "system",
+            content: [
+              "You are an AI assistant from TryMailGPT.com responding to questions sent to you via email.",
+              "Reply to the question as an email thread.",
+              "Do not include From, To, or Subject headers in your response.",
+              "Do not include Best Regards or other salutations at the end of your response.",
+              "Take a deep breath. You got this!"
+            ].join(" ")
+          },
+          { role: "user", content: prompt }
+        ]
       }
     )
 
     @prompt = prompt
     @response = response["choices"].map { _1["message"]["content"] }.join("\n")
 
-    mail(to: from, subject: "RE: #{subject}", body: @response)
+    mail(
+      to: from,
+      subject: "Re: #{subject}",
+      body: [
+        @response,
+        prompt.split("\n").map { "> #{_1}" }.join("\n")
+      ].join("\n\n")
+    )
   end
 end
